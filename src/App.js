e9746dacd424a1a473b32d3c54b8b5638a6c82f5
lenkon <inoulenkon@gmail.com>
2023-03-01 22:14:24 +0530

    Create a full website with Home, Calculator and Quote pages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Calculator from './components/Calculator';
+import Home from './components/Home';
+import Quote from './components/Quote';
+import NavBar from './components/NavBar';
 
 class App extends React.Component {
   constructor(props) {
@@ -9,9 +13,15 @@ class App extends React.Component {
 
   render() {
     return (
-      <div className="app-container">
-        <Calculator />
-      </div>
+      <BrowserRouter>
+        <NavBar />
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/math-magician" element={<Home />} />
+          <Route path="/calculator" element={<Calculator />} />
+          <Route path="/quote" element={<Quote />} />
+        </Routes>
+      </BrowserRouter>
     );
   }
 }
